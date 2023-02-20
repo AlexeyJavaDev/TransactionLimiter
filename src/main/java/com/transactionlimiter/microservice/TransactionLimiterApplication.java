fcf51bc0ea3e3989797b83f9ee15f23db829e307
Alexey Grishin <alexeymrjava@gmail.com>
2023-02-20 18:39:51 +0600

    Initial version

---
+++ src/main/java/com/transactionlimiter/microservice/TransactionLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.transactionlimiter.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transactionlimiter/microservice/TransactionLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactionlimiter.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
